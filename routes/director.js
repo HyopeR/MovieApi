5c5856fae0e855a9db0e571b3387aed43c7aaeff
HyopeR <tol_celik@hotmail.com>
2020-03-26 05:23:59 +0300

    /api/directors/:director_id GET

--- routes/director.js
+++ routes/director.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
@@ -6,6 +7,59 @@ const Director = require('../models/Director');
 
 router.get('/', (req, res) => {
   const promise = Director.aggregate([
+    {
+      $lookup: {
+        from: 'movies',
+        localField: '_id',
+        foreignField: 'director_id',
+        as: 'director_movies'
+      }
+    },
+    {
+      $unwind: {
+        path: '$director_movies',
+        // Joinlenmeyen directorları gösterme.
+        preserveNullAndEmptyArrays: true,
+
+      }
+    },
+    {
+      $group: {
+        _id: {
+          _id: '$_id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio',
+        },
+        director_movies: {
+          $push: '$director_movies'
+        }
+      }
+    },
+    {
+      $project: {
+        _id: '$_id._id',
+        name: '$_id.name',
+        surname: '$_id.surname',
+        director_movies: '$director_movies'
+      }
+    }
+  ]);
+
+  promise.then((data) => {
+    res.json(data);
+  }).catch((error) => {
+    res.json(error);
+  });
+});
+
+router.get('/:director_id', (req, res) => {
+  const promise = Director.aggregate([
+    {
+      $match: {
+        '_id': mongoose.Types.ObjectId(req.params.director_id)
+      }
+    },
     {
       $lookup: {
         from: 'movies',
