fb63a6550871742b1fba9d9d2a499a354c90b58f
HyopeR <tol_celik@hotmail.com>
2020-03-28 03:37:15 +0300

    test POST movies

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -34,4 +34,33 @@ describe('/api/movies tests', () => {
            done();
         });
     });
+
+    describe('/POST movie', () => {
+        it('It should POST a movie', (done) => {
+            const movie = {
+                title: 'Udemy',
+                director_id: '5e7c09cafea21805903e5521',
+                category: 'Comedy',
+                country: 'Turkey',
+                year: 1950,
+                imdb_score: 8
+            };
+
+            chai.request(server)
+                .post('/api/movies')
+                .send(movie)
+                .set('x-access-token', token)
+                .end((error, res) => {
+                   res.should.have.status(200);
+                   res.body.should.be.a('object');
+                   res.body.should.have.property('title');
+                   res.body.should.have.property('director_id');
+                   res.body.should.have.property('category');
+                   res.body.should.have.property('country');
+                   res.body.should.have.property('year');
+                   res.body.should.have.property('imdb_score');
+                   done();
+                });
+        });
+    });
 });
